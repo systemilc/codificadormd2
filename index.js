47b2fd55a8c724c6713b55c8a8bd28271b23f661
systemilc <isaac.systemilc@gmail.com>
2022-05-03 11:10:59 -0300

    Alterado botao | incluído função ocultar passos

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+
 let botao = document.querySelector('#enviarBtn')
 let radioBtns = document.querySelectorAll("input[name='tipoCodificacao']");
 let resultado = document.getElementById('enviarBtn')
-let selecao = document.querySelector('.ocultarPasso').value
+let showPassos = document.querySelector('.chaves')
 
 let radioSelecionado = () => {
     let selecionado = document.querySelector('input[name="tipoCodificacao"]:checked').value;
   resultado.textContent = `${selecionado}`
   botao.style.display = "block"
-
 }
 
 botao.addEventListener('click', function(event){
@@ -21,15 +21,13 @@ radioBtns.forEach(radioBtns => {
 })
 radioSelecionado()
 
-
-var selecionar = document.addEventListener('change',  function(){
-
-    
-    if(selecao === "cifraDeCesar"){
-        showPassos.style.display = "block"
-        showPassos.style.flexDirection = 'column'
-    
-    }else{
+function selecionado(value){
+    var dadosLoja = document.getElementsByClassName('dadosLoja');
+        if(value === "cifraDeCesar"){
+            showPassos.style.display = "flex"
+           showPassos.style.flexDirection = 'column'
+      }else{
         showPassos.style.display = 'none'
+      }
     }
-    })
+
